1d50d1462cb6d7821ef1c14cb58a1904b7737810
MaleiE <co0n@tut.by>
2017-11-30 18:04:00 +0300

    +

---
+++ src/main/java/com/malei/card/api/demo/dto/CardIdUserIdDto.java
@@ -0,0 +1,23 @@
+package com.malei.card.api.demo.dto;
+
+
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@ToString
+public class CardIdUserIdDto {
+    private String userId;
+    private String cardId;
+
+    public CardIdUserIdDto(Long userId, Long cardId) {
+        this.userId = userId.toString();
+        this.cardId = cardId.toString();
+    }
+
+    public CardIdUserIdDto() {
+    }
+
+}
---
+++ src/main/java/com/malei/card/api/demo/exception/EntityNotFoundException.java
@@ -0,0 +1,12 @@
+package com.malei.card.api.demo.exception;
+
+
+public class EntityNotFoundException extends RuntimeException {
+
+    public EntityNotFoundException() {
+    }
+
+    public EntityNotFoundException(String message) {
+        super(message);
+    }
+}
